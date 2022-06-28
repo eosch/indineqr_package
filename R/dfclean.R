ceb128ee3257b8e07b9f66ffb249e0bd3386f82e
Eli Schaub <es3870a@student.american.edu>
2022-06-28 16:23:48 -0400

    Fixed some issues with the data cleaning function and added some clarifying comments

--- R/dfclean.R
+++ R/dfclean.R
@@ -1,10 +1,14 @@
 library(tidyverse)
+# function aims to rearrange the data in df by the variable measuring inequality (ineqvar) in a specified order
 dfclean<-function(ineqvar, df, order){
-  if (order="l2h") {
-    df |>
-      arrange(ineqvar)->df
-  } else if (order="h2l"){
-    df |>
-      desc(ineqvar)->df
+  # ineqvar must be input with quotations to work
+  # df is relevant dataframe
+  # order is either "l2h" for low to high or "h2l" for high to low
+  if (order == "l2h") {
+    df|>
+      arrange(df[[ineqvar]])
+  } else if (order == "h2l"){
+    df|>
+      arrange(desc(df[[ineqvar]]))
   }
 }
