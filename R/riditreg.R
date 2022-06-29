e6bdae41115df283ad2422c3b3f6e3aad44c41b1
Eli Schaub <es3870a@student.american.edu>
2022-06-29 17:48:38 -0400

    Added logistic regression as an option

--- R/riditreg.R
+++ R/riditreg.R
@@ -1,10 +1,15 @@
-# This function aims to take the cleaned/organized data and return relevent regression models
-# relies on lm() and glm() from stats and if negative binomial is an option may have to rely on glm.nb() from MASS
+# This function aims to take the cleaned/organized data and return relevant regression models
+# relies on lm() and glm() from stats
 library(MASS)
-riditreg<-function(df, yvar, method, offsetvar){
+riditreg<-function(df, yvar, method, offsetvar, countvar){
   # df is dataframe
   # yvar is the dependent variable being modeled (must be in quotations)
-  # method is the chosen regression method with options "lm" for simple linear regression and "poisson" for poisson regression
+  # method is the chosen regression method with options:
+  #   "lm" for simple linear regression
+  #   "poisson" for poisson regression
+  #   "nb" for negative binomial regression
+  #   "logistic" for logistic regression
+  # offsetvar is the is the variable used in the offset for poisson and nb
   if (method == "lm"){
     reg<-lm(df[[yvar]]~ridit, df)
     summary(reg)
@@ -15,6 +20,7 @@ riditreg<-function(df, yvar, method, offsetvar){
     reg<-glm.nb(df[[yvar]]~ridit+offset(log(df[[offsetvar]])))
     summary(reg)
   } else if (method == "logistic"){
-
+    reg<-glm(df[[countvar]]/df[[offsetvar]]~ridit, data = df, family = "binomial", weights = df[[offsetvar]])
+    summary(reg)
   }
 }
