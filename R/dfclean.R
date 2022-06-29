da21d95acd8122fe7fffb1144d0cf6f6cc7d36eb
Eli Schaub <es3870a@student.american.edu>
2022-06-29 17:46:30 -0400

    Renamed countvar to offsetvar for consistency

--- R/dfclean.R
+++ R/dfclean.R
@@ -1,11 +1,11 @@
 library(tidyverse)
 # This function aims to rearrange the data in df by the variable measuring inequality (ineqvar) in a specified order
 # Additionally, it creates new variables for frequencies and ridit
-dfclean<-function(ineqvar, df, order, countvar){
+dfclean<-function(ineqvar, df, order, offsetvar){
   # ineqvar must be input with quotations to work and is the variable measuring inequality
   # df is relevant dataframe
   # order is either "l2h" for low to high or "h2l" for high to low based on how the inequality measurement needs to be arranged
-  # countvar is the variable with counts used for creating the frequencies and the ridit
+  # offsetvar is the variable with counts used for creating the frequencies and the ridit
 
   # rearranging by inequality
   if (order == "l2h") {
@@ -17,14 +17,14 @@ dfclean<-function(ineqvar, df, order, countvar){
   }
 
   # creating frequency vector
-  freq<-vector(mode = "numeric", length = length(df[[countvar]]))
-  for (i in 1:length(df[[countvar]])){
-    freq[i]=(df[[countvar]][i])/(sum(df[[countvar]]))
+  freq<-vector(mode = "numeric", length = length(df[[offsetvar]]))
+  for (i in 1:length(df[[offsetvar]])){
+    freq[i]=(df[[offsetvar]][i])/(sum(df[[offsetvar]]))
   }
 
   # creating cumulative frequency vector
-  cumfreq<-vector(mode = "numeric", length = length(df[[countvar]]))
-  for (j in 1:length(df[[countvar]])){
+  cumfreq<-vector(mode = "numeric", length = length(df[[offsetvar]]))
+  for (j in 1:length(df[[offsetvar]])){
     if (j == 1){
       cumfreq[j]=freq[j]
     } else {
@@ -33,8 +33,8 @@ dfclean<-function(ineqvar, df, order, countvar){
   }
 
   # creating ridit vector
-  ridit<-vector(mode = "numeric", length = length(df[[countvar]]))
-  for (k in 1:length(df[[countvar]])){
+  ridit<-vector(mode = "numeric", length = length(df[[offsetvar]]))
+  for (k in 1:length(df[[offsetvar]])){
     if (k == 1){
       ridit[k]=cumfreq[k]/2
     } else {
