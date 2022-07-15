ea5be63cc598c540541b149b50fa88c51a414e01
Eli Schaub <es3870a@student.american.edu>
2022-07-15 19:37:47 -0400

    Added an example df

---
+++ R/dfexample.R
@@ -0,0 +1,14 @@
+#' Example data from Colombia
+#'
+#' Source: Pan American Health Organization
+#'
+#' @format A data frame with 5 variables: \code{municipios}, \code{MN},
+#'   \code{NV}, \code{TMN}, \code{IDSx}
+#' \describe{
+#' \item{municipios}{The name of the municipality}
+#' \item{MN}{The number of stillbirths}
+#' \item{NV}{The number of live births}
+#' \item{TMN}{The rate of stillbirths per 1000 births}
+#' \item{IDSx}{Sustainable Development Index, a measure of social inequality}
+#' }
+"dfexample"
